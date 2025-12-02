68a9d9a5fe07f409a807f54bf9ffc173d9a5c278
Yitzchak Scott-Thoennes <sthoenna@gmail.com>
2025-12-02 12:30:13 -0500

    2025 day 2

---
+++ 2025/day2b.pl
@@ -0,0 +1,14 @@
+use 5.036;
+# day 2 https://adventofcode.com/2025/day/2
+# input is a comma separated list of low-high numeric ranges
+# sum the numbers in the ranges are of once or more repeated digits (999, 1010, etc.)
+@ARGV = 'day2.txt' unless @ARGV;
+chomp(my $ranges = <>);
+my $sum = 0;
+for my $range (split /,/, $ranges) {
+    my ($from, $to) = split /-/, $range;
+    for my $n ($from..$to) {
+        $sum += $n if $n =~ /^(.*)\1+\z/;
+    }
+}
+say $sum;
