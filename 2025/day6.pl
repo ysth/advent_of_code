9ab875a26854f11b72f426352e7c999fb4e00484
Yitzchak Scott-Thoennes <sthoenna@gmail.com>
2025-12-06 23:00:29 -0500

    2025 day 6

---
+++ 2025/day6.pl
@@ -0,0 +1,11 @@
+use 5.036;
+# day 6 https://adventofcode.com/2026/day/6
+@ARGV = 'day6.txt' unless @ARGV;
+
+my @data = map [split], <>;
+my $sum = 0;
+my $ops = pop @data;
+while (my $op = shift @$ops) {
+    $sum += eval join $op, map shift @$_, @data;
+}
+say $sum;
---
+++ 2025/day6b.pl
@@ -0,0 +1,20 @@
+use 5.036;
+# day 6 https://adventofcode.com/2026/day/6
+@ARGV = 'day6.txt' unless @ARGV;
+
+chomp(my @data = <>);
+my @ops = split ' ', pop @data;
+
+my $sum = 0;
+while (my $op = shift @ops) {
+    my @operands;
+    {
+        my $operand = join '', map substr($_, 0, 1, ''), @data;
+        if ($operand =~ /\d/a) {
+            push @operands, $operand;
+            redo;
+        }
+    }
+    $sum += eval join $op, @operands;
+}
+say $sum;
